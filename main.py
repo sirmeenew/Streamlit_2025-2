e6d6dbf2d3a3c25cb1e4342db2bfcab2799638b4
sirmeenew <canadi1029@gmail.com>
2025-12-15 15:07:22 +0900

    Enhance K-pop analysis with data processing and visualization
    Added data processing and visualization for K-pop analysis.

--- main.py
+++ main.py
@@ -8,3 +8,65 @@ st.set_page_config(
 
 st.sidebar.title('K-pop Demon Hunters 분석')
 
+with st.echo():
+  import pandas as pd
+from itertools import combinations
+from collections import Counter
+import networkx as nx
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import re
+from konlpy.tag import Okt
+
+df = pd.read_csv('./kdh_newsdata.csv')
+
+headline = ''.join(df['title'].tolist())
+headline
+
+def cleanString(text):
+  pattern = r'<[^>]*>'
+  text = re.sub(pattern=pattern, repl='', string=text)
+  
+  pattern = r'[^\w\s\n]'
+  text = re.sub(pattern=pattern, repl='', string=text)
+  
+  return text
+
+from matplotlib import font_manager
+
+han_font_path = font_manager.findfont('Malgun Gothic')
+
+s = open('../Stopwords/stopwords.txt','r')
+stopwords = s.read().splitlines()
+print(stopwords)
+
+headline_cleaned = cleanString(headline)
+
+okt = Okt()
+
+words_morphs = okt.morphs(headline_cleaned)
+
+words = [word for word in words_morphs if word not in stopwords]
+print(words)
+
+descriptions = ''.join(df['description'].tolist())
+
+descriptions_cleaned = cleanString(descriptions)
+
+okt_2 = Okt()
+
+descriptions_morphs = okt_2.morphs(descriptions_cleaned)
+
+words_des = [word for word in descriptions_morphs if word not in stopwords]
+print(words_des)
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+wordsdes = WordCloud().generate(descriptions_cleaned)
+print(wordsdes.words_)
+
+wordshead = WordCloud().generate(headline_cleaned)
+print(wordshead.words_)
+
+plt.imshow(wordsdes)
